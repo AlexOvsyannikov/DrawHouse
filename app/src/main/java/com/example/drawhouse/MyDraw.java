0c440131ac72637e0fc29edf9c1b854a4c5aa732
AlexOff <alexov2004@gmail.com>
2021-01-04 16:53:47 +0300

    MyDraw class added

---
+++ app/src/main/java/com/example/drawhouse/MyDraw.java
@@ -0,0 +1,16 @@
+package com.example.drawhouse;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class MyDraw extends View {
+    public MyDraw (Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
